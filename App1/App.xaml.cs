c9e1814fe6254288dceff26757c102d5670d5b44
hu.junmeng <hu.junmeng@jiangxi-isuzu.cn>
2017-07-06 23:08:48 +0800

    121212

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -95,6 +95,8 @@ namespace App1
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: 保存应用程序状态并停止任何后台活动
             deferral.Complete();
+            int i = 1;
+
         }
     }
 }
